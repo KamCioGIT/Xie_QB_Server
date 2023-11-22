5a6ee519ae29400dfb6e9bdb0bfa499ff30f0d1a
SuperMan-Sam <31952622+SuperMan-Sam@users.noreply.github.com>
2023-11-21 23:07:20 -0500

    1

---
+++ [Lock-Map]/izzy-truefalse-map/fxmanifest.lua
@@ -0,0 +1,10 @@
+this_is_a_map "yes"
+author 'Melanom'
+description 'True or False'
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+escrow_ignore {}
+
+dependency '/assetpacks'
---
+++ [Lock-Map]/j17_firepole/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'jay17 Modifications'
+description 'Fire Pole Script'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+dependency '/assetpacks'
